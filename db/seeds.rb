a31203a1311c8ef8aff73f51bf022e5082f08bc9
TylerDicks <tdicks@cedarville.edu>
2016-04-29 13:40:00 -0400

    Got through step 15 of thing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.new
+user1.login = "joe"
+user1.email = "joe@cedarville.edu"
+user1.password = "password"
+user1.password_confirmation = "password"
+user1.save!
+
+user2 = User.new
+user2.login = "pete"
+user2.email = "pete@cedarville.edu"
+user2.password = "password"
+user2.password_confirmation = "password"
+user2.save!
+
+#give joe a couple of plans
+Plan.create(user_id: user1.id, name: "Plan1")
+Plan.create(user_id: user2.id, name: "Plan2")
